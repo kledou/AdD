59e4da7b3aa45e183b0a55667d656bcf290d5f14
Khaled <khaled.larbi@live.fr>
2018-11-01 23:05:54 +0100

    Ajout des vérifications sur le variable identificatrice

--- R/acp.R
+++ R/acp.R
@@ -7,4 +7,15 @@ ACP <- function(DATAACT, DATASUP = NULL, VARACT, VARSUP = NULL,ID = NULL, IDSUP 
       stop("Il existe des variables non quantitatives parmi les variables actives : cela peut être dû à un problème de lecture de la table.
            Si ce n'est pas le cas, il vous est conseillé de vous orienter vers d'autres méthodes comme l'ACM ou l'AFDM.")
     }
+
+    if(!is.null(ID)){
+      if(!(ID %in% colnames(DATAACT))){
+        warning("Le nom de variable ID ne correspond pas à une colonne de la matrice DATAACT.
+                Les individus seront identifiés à l'aide de leurs numéros de ligne.")
+      } else {
+        rownames(DATAACT) <- DATAACT[,ID]
+        DATAACT <- DATAACT[,colnames(DATAACT) != ID]
+      }
+    }
   }
+}
